d78832d905f4c09027d413c9437f0a0669f53566
Anders Nissen Mosegaard <155452275+amosegaard-foreflight@users.noreply.github.com>
2024-06-04 13:38:42 +0200

    Fix random crash

--- Sources/FF-WeatherHelper/Helpers/Endpoints.swift
+++ Sources/FF-WeatherHelper/Helpers/Endpoints.swift
@@ -1,24 +1,26 @@
 import Foundation
 
 struct EndPoints {
-    private static var components: URLComponents = {
+    private static func createComponents() -> URLComponents {
         var c = URLComponents()
         c.scheme = "https"
         c.host = "qa.foreflight.com"
         return c
-    }()
+    }
 
     static func weatherReport(identifier: String) throws -> URLRequest {
+        var components = createComponents()
         components.path = "/weather/report/"
         
-        guard let url = components.url?.appending(path: identifier.lowercased() ) else {
+        guard let url = components.url?.appendingPathComponent(identifier.lowercased()) else {
             throw RequestError.invalidUrl
         }
         
-        var request = URLRequest(url: url )
+        var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.setValue("1", forHTTPHeaderField: "ff-coding-exercise")
 
         return request
     }
 }
+
